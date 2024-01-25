15ca36ca2e45ff145b7e07308fe8715c99e00b8c
yousefnegmeldin <yousefnegm@gmail.com>
2024-01-25 18:19:03 +0200

    added mobile responsiveness and tweaked card

--- src/components/CodeList.jsx
+++ src/components/CodeList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 
 import codeCards from '../data/cards'
 import CodeCard from './CodeCard'
@@ -6,10 +6,10 @@ import CodeCard from './CodeCard'
 const CodeList = () => {
   return (
     <div className='w-full flex flex-col items-center justify-center p-4'> 
-        <div className='text-6xl text-slate-200'>
+        <div className='mt-4  text-4xl text-center lg:text-6xl text-slate-200'>
             Welcome to my React Playground!
         </div>
-        <div className='w-4/5 text-white pt-12 grid grid-cols-3'>
+        <div className='w-4/5 text-white pt-12 grid lg:grid-cols-3 grid-cols-1'>
             {codeCards.map((card)=>
               <CodeCard 
                key={card.cardNumber}
